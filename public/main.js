eb12116db42b2bf26ed487353b2c68e0b410fbb1
Julie Tran <tranjhdesign@gmail.com>
2021-03-19 00:12:16 -0400

    Add thumbs down functionality
    * remove unused index.html
    * restyle background color for medical purposes

--- public/main.js
+++ public/main.js
@@ -1,45 +1,81 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
+const thumbDown = document.getElementsByClassName(`js-thumbs-down`);
+
+// makes all of the thumbDown buttons clickable
+Array.from(thumbDown).forEach((element) => {
+  element.addEventListener(`click`, onThumbDown);
 });
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+function onThumbDown(clickEvent) {
+  changeThumbValue(clickEvent, -1)
+}
+
+// ------------------------------------------------------------------------------------------------------------
+const thumbUp = document.getElementsByClassName(`js-thumbs-up`);
+
+// makes all of the thumbUp buttons clickable
+Array.from(thumbUp).forEach((element) => {
+  element.addEventListener(`click`, onThumbUp);
 });
+
+function onThumbUp(clickEvent) {
+  changeThumbValue(clickEvent, 1)
+}
+
+// ------------------------------------------------------------------------------------------------------------
+const trash = document.getElementsByClassName(`js-trash`);
+
+Array.from(trash).forEach((element) => {
+  element.addEventListener(`click`, onTrashClicked)
+});
+
+function onTrashClicked(event) {
+  const name = event.target.parentNode.parentNode.childNodes[1].innerText
+  const msg = event.target.parentNode.parentNode.childNodes[3].innerText
+  fetch(`messages`, {
+    method: `DELETE`,
+    headers: {
+      'Content-Type': `application/json`
+    },
+    body: JSON.stringify({
+      name: name,
+      msg: msg
+    })
+  }).then((response) => response.json()).then((response) => {
+    window.location.reload()
+    console.log(response)
+  })
+}
+
+// utility functions
+function changeThumbValue(clickEvent, newThumbValue) {
+  // get the clicked element
+  const clickedElement = clickEvent.target
+
+  // get the message associated with the clicked element (closest message)
+  const message = clickedElement.closest(`.message`)
+
+  // get the message info from inside the message div
+  const name = message.querySelector(`.name`).innerText
+  const msg = message.querySelector(`.msg`).innerText
+  const thumbUp = parseInt(message.querySelector(`.thumbUp`).innerText)
+
+  // we hit the /messages endpoint of our own server
+  fetch(`messages`, {
+    // we are performing an update
+    method: `PUT`,
+    // our content will be JSON, or a javascript object
+    headers: { 'Content-Type': `application/json` },
+    // we send the information from the DOM
+    body: JSON.stringify({
+      name: name,
+      msg: msg,
+      thumbUp: thumbUp + newThumbValue
+    })
+  }).then(response => {
+    if (response.ok) return response.json()
+  }).then(data => {
+    console.log(data)
+    window.location.reload()
+  })
+}
--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-var db, collection;
+let db;
 
 const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
 const dbName = "demo";
 
 app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, mongoClient) => {
+    if (error) {
+      throw error;
+    }
+    db = mongoClient.db(dbName);
+    console.log("Connected to `" + dbName + "`!");
+  });
 });
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.json({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   db.collection('messages').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', { messages: result })
   })
 })
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+  db.collection('messages').insertOne({ name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown: 0 }, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
@@ -40,22 +39,22 @@ app.post('/messages', (req, res) => {
 
 app.put('/messages', (req, res) => {
   db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({ name: req.body.name, msg: req.body.msg }, {
+      $set: {
+        thumbUp: req.body.thumbUp
+      }
+    }, {
+      sort: { _id: -1 },
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
 })
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+  db.collection('messages').findOneAndDelete({ name: req.body.name, msg: req.body.msg }, (err, result) => {
     if (err) return res.send(500, err)
-    res.send('Message deleted!')
+    res.send({ result: 'Message deleted!', info: result })
   })
 })
